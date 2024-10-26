6c2bb124380a29dc0a5fb51754d15d4e42287c47
eduVaya <evalladarespacheco@protonmail.com>
2024-10-26 00:09:44 -0400

    set frontend with dummy data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,19 @@
-import React from 'react'
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Outlet } from "react-router-dom";
 
 const App = () => {
   return (
-    <h1>WELCOME TO proSHOPS</h1>
+    <>
+      <Header />
+      <main>
+        <Container>
+          <Outlet />
+        </Container>
+      </main>
+      <Footer />
+    </>
   )
 }
 
